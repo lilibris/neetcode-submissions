08e3e88e39a2687aea5b326d5f95831f89bd7390
Li Li <53863463+lilibris@users.noreply.github.com>
2026-08-14 10:22:09 +0100

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        #print(" ".join(strs))
+        return " ".join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        #print(s.split())
+        return s.split() if len(s)>1 else [""]
