fa61ed45b9bfde0f5b059546452ec93dec034fc5
Li Li <53863463+lilibris@users.noreply.github.com>
2026-08-14 12:31:04 +0100

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # length@ as dlimeter
+        if not strs or strs == [""]: # []
+            return str(strs)
+        rtn = "".join(f"{len(word)}@{word}" for word in strs)
+        
+        return rtn
+
+    def decode(self, s: str) -> List[str]:
+        if s == "[]":
+             return []
+        elif s == "['']":
+             return [""]
+        rtn, i = [], 0
+        while i < len(s):
+            right = i
+            # find the delimiter first:
+            while s[right] != "@":
+                right += 1
+            word_len = int(s[i:right])
+            rtn.append(s[right+1:right+1+word_len])
+            i = right+1+word_len
+        return rtn
+
+
+
+        
